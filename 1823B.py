d9a1435f8043fd38533c8fdc72ecb6c68bca2cd9
HenriSrv <sarevhenri@gmail.com>
2023-05-02 15:30:41 +0100

    fixed error with weird value changing - used .copy()

--- 1823B.py
+++ 1823B.py
@@ -3,36 +3,45 @@
 n_tests = int(input())
 
 
-def solve(n, k, p):
-    target = sorted(p, reverse=False)
-    if p == target:  # if list already sorted, return 0 immediately
+def solve(n, k, arr):
+    target = sorted(arr, reverse=False)
+    if arr == target:  # if list already sorted, return 0 immediately
         return 0
 
     for j in range(n):  # compare each i and (i + k)th element to see if they can be swapped
         for i in range(n-k):
-            if p[i] > p[i + k]:
-                p[i], p[i + k] = p[i + k], p[i]
+            if arr[i] > arr[i + k]:
+                arr[i], arr[i + k] = arr[i + k], arr[i]
 
-    if p == target:
+    if arr == target:
         return 0
 
     count = 0
     while count <= 2:
         for i in range(n):
-            if p[i] != target[i]:
+            if arr[i] != target[i]:
                 count += 1
         if count <= 2:  # if this is true, then exactly two elements are misplaced .'. only 1 pre-swap is required
             return 1
     return -1
 
-# currently fails when 1 swap would allow the remaining swaps to occur properly. eg 4 2: 3 1 4 2
-# if you swapped the 1 and 4 you would have 3 4 1 2, which can then be sorted with the step k (2)
-
 
 def main():
     n, k = map(int, input().split())
     p = list(map(int, input().split()))
-    return solve(n, k, p)
+    tempP = p.copy()
+    result = solve(n, k, tempP)
+    if result != -1:
+        return result
+    else:
+        for i in range(n):
+            for j in range(n):
+                temp = p.copy()
+                temp[i], temp[j] = temp[j], temp[i]
+                result = solve(n, k, temp)
+                if result == 0:
+                    return 1
+        return -1
 
 
 while n_tests > 0:
